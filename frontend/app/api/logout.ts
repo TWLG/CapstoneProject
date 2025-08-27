b42a9d1dcffb6bc15b6dfccb68911f7603ec452c
Liam Coyle <TWLGGLES@outlook.com>
2025-08-26 21:06:07 -0400

    aaa

--- frontend/app/api/logout.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { clearTokenCookie } from '../lib/auth';
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-clearTokenCookie(res);
-res.status(200).json({ ok: true });
-}
